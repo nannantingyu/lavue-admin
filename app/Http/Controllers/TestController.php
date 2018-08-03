04f970baddb15383ef7c5baf8ed8bd1a6c8ff7da
wangxinyao <xinyao.wang@mosoink.com>
2018-08-03 17:06:13 +0800

    提交

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,15 +6,6 @@ use App\Models\Category;
 
 class TestController extends Controller {
     public function index() {
-//        dd(Article::find(1)->body);
-//        DB::enableQueryLog();
-        $articles = Category::find(1)->articles;
-        dump($articles);
-        $article = Article::find(1);
-        dump($article->categories);
-        dump($article->comments);
-
-//        dd(DB::getQueryLog());
-//        dd(12345);
+        $str = "aabbbcccddeefff";
     }
 }
