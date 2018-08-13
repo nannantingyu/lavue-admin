29016402fc3754fa1aac7c992bfd0e02b7db30e7
杨吉 <yangji@91guoxin.com>
2018-08-13 16:36:33 +0800

    修改更多新闻接口

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -23,25 +23,45 @@ class ArticleController extends Controller
 
         $time = $request->input('time');
         $where = "";
-
+        $sql = '';
         if (is_null($typeId)) {
-            $idsStr = $this->getFilter();
-            $where = "where cid not in (select jujin8_category.id from jujin8_category where jujin8_category.ename in ($idsStr))";
+            $sql = 'SELECT a.id,title,publish_time AS created,keywords AS catTitle,description AS metadesc,image AS imgurl,author AS author,source_site AS sourceSite,source_url AS sourceUrl FROM jujin8_article a INNER JOIN jujin8_article_body as body ON a.id = body.aid ';
+            $ids = $this->getFilterIds();
+            $where = " WHERE aid not in ($ids) ";
         } else {
-            $where = "where cid = $typeId";
+            $where = " where cid = $typeId ";
+            $sql = "SELECT a.id,title,publish_time AS created,keywords AS catTitle,description AS metadesc,image AS imgurl,author AS author,source_site AS sourceSite,source_url AS sourceUrl FROM jujin8_article a LEFT JOIN jujin8_article_category ac ON a.id=aid  INNER JOIN jujin8_article_body as body ON a.id = body.aid ";
         }
 
         if (!is_null($time)) {
-            $where = "$where and publish_time < '$time'";
+            $where = "$where and publish_time < '$time' AND state = 1";
         }
 
-        $sql = "SELECT distinct jujin8_article.id,title,publish_time AS created,keywords AS catTitle,description AS metadesc,image AS imgurl,author AS author,source_site AS sourceSite,source_url AS sourceUrl,source_type as sourceType FROM jujin8_article RIGHT JOIN jujin8_article_category ON jujin8_article.id=aid $where ORDER BY publish_time DESC LIMIT $size";
+        $sql = "$sql $where ORDER BY publish_time DESC LIMIT $size";
+
         $lists = DB::select($sql);
 
         for ($i = 0; $i < count($lists); $i++) {
             $lists[$i]->imgurl = str_replace('uploads/crawler', 'uploads', $lists[$i]->imgurl);//a或b或c都替换成a
         }
         return ['success' => 1, 'data' => $lists];
+    }
+
+    private function getFilterIds()
+    {
+        $idsStr = $this->getFilter();
+        $sql = "SELECT aid as id FROM jujin8_article_category where cid in (SELECT id FROM jujin8_category WHERE ename in ($idsStr))";
+
+        $list = DB::select($sql);
+
+        $ids = '';
+        for ($i = 0; $i < count($list); $i++) {
+            if ($i != 0) {
+                $ids = $ids . ',';
+            }
+            $ids = $ids . $list[$i]->id;
+        }
+        return $ids;
     }
 
     private function getFilter()
