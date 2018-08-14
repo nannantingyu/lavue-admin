71cb15c9cd7bd7a0ccf36036c620c5a7aa8fe856
lianglili <lianglili@jujin8.com>
2018-08-14 17:54:15 +0800

    财经日历

---
+++ resources/assets/js/page/event.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "event"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/assets/js/page/holiday.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "holiday"
+    }
+</script>
+
+<style scoped>
+
+</style>
