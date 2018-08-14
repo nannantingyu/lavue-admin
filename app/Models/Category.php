af1ee1a67863032e6f3906345156bd9c09c1c415
yangmingming <yangmingming@jujin8.com>
2018-08-14 18:05:18 +0800

    添加字段

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model {
     protected $table = 'category';
-    protected $fillable = ['name', 'ename', 'target', 'pid', 'sequence', 'state'];
+    protected $fillable = ['name', 'ename', 'target', 'pid', 'sequence', 'state', 'type'];
 
     public function articles() {
         return $this->belongsToMany("App\Models\Article", 'article_category', 'cid', 'aid');
